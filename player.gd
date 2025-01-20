95d2ddd50d2d6d69afd801a27408b38258075292
SnappyDragon64 <pg61151@gmail.com>
2025-01-20 13:35:31 +0530

    Player Movement State Machine

--- player.gd
+++ player.gd
@@ -1,54 +1,139 @@
 extends CharacterBody3D
 
 
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 4.5
-@export var GRAVITY = 1.0
+enum MovementState {IDLE, RUN, JUMP, FALL, COYOTE_TIME, JUMP_QUEUED}
 
-var can_jump = false
-var coyote = false
-var jump_queued = false
+@export_group("Movement")
+@export var SPEED := 5.0
+@export var JUMP_SPEED := 4.5
+@export var GRAVITY := 1.0
+
+var movement_state: MovementState = MovementState.IDLE: set = _set_movement_state
+var input_direction := 0.0
 
 
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity += get_gravity() * GRAVITY * delta
-		
-		if not coyote:
-			coyote = true
-			$CoyoteTimer.start()
-
-	if is_on_floor():
-		can_jump = true
-		coyote = false
+func _set_movement_state(new_movement_state: MovementState):
+	match movement_state:
+		MovementState.COYOTE_TIME:
+			$CoyoteTimer.stop()
+		MovementState.JUMP_QUEUED:
+			$JumpQueueTimer.stop()
 	
-	if Input.is_action_just_pressed("jump"):
-		if can_jump:
-			can_jump = false
-			velocity.y = JUMP_VELOCITY
-		else:
-			jump_queued = true
+	match new_movement_state:
+		MovementState.JUMP:
+			velocity.y = JUMP_SPEED
+		MovementState.COYOTE_TIME:
+			$CoyoteTimer.start()
+		MovementState.JUMP_QUEUED:
 			$JumpQueueTimer.start()
 	
-	if Input.is_action_pressed("jump") and can_jump and jump_queued:
-		can_jump = false
-		velocity.y = JUMP_VELOCITY
+	movement_state = new_movement_state
+
+
+func _physics_process(delta) -> void:
+	input_direction = Input.get_axis("move_left", "move_right")
 	
-	if Input.is_action_just_released("jump") and velocity.y > 0.0:
-		velocity.y = 0.0
-
-	var input_dir = Input.get_axis("move_left", "move_right")
-	if input_dir:
-		velocity.x = input_dir * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	match movement_state:
+		MovementState.IDLE:
+			_idle_physics_process(delta)
+		MovementState.RUN:
+			_run_physics_process(delta)
+		MovementState.JUMP:
+			_jump_physics_process(delta)
+		MovementState.FALL:
+			_fall_physics_process(delta)
+		MovementState.COYOTE_TIME:
+			_coyote_time_physics_process(delta)
+		MovementState.JUMP_QUEUED:
+			_jump_queued_physics_process(delta)
+	
+	#print_debug("Movement State: ", MovementState.keys()[movement_state])
+	
 	move_and_slide()
 
 
+func _idle_physics_process(delta) -> void:
+	if input_direction:
+		movement_state = MovementState.RUN
+	
+	_handle_jump()
+	_handle_coyote_time()
+
+func _run_physics_process(delta) -> void:
+	if not input_direction:
+		movement_state = MovementState.IDLE
+	
+	_handle_x_movement()
+	_handle_jump()
+	_handle_coyote_time()
+
+
+func _jump_physics_process(delta) -> void:
+	_handle_gravity(delta)
+	_handle_x_movement()
+	
+	if velocity.y <= 0.0:
+		movement_state = MovementState.FALL
+
+
+func _fall_physics_process(delta) -> void:
+	_handle_gravity(delta)
+	_handle_x_movement()
+	_handle_land()
+	
+	if Input.is_action_just_pressed("jump"):
+		movement_state = MovementState.JUMP_QUEUED
+
+
+func _coyote_time_physics_process(delta) -> void:
+	_handle_gravity(delta)
+	_handle_x_movement()
+	_handle_jump()
+	_handle_land()
+
+
+func _jump_queued_physics_process(delta) -> void:
+	_handle_gravity(delta)
+	_handle_x_movement()
+	_handle_land()
+	
+	if Input.is_action_pressed("jump"):
+		if is_on_floor():
+			movement_state = MovementState.JUMP
+	else:
+		movement_state = MovementState.FALL
+
+
+func _handle_gravity(delta) -> void:
+	velocity += get_gravity() * GRAVITY * delta
+
+
+func _handle_x_movement() -> void:
+	var input_dir = Input.get_axis("move_left", "move_right")
+	velocity.x = input_dir * SPEED
+
+
+func _handle_jump() -> void:
+	if Input.is_action_just_pressed("jump"):
+		movement_state = MovementState.JUMP
+
+
+func _handle_land() -> void:
+	if is_on_floor():
+		if input_direction:
+			movement_state = MovementState.RUN
+		else:
+			movement_state = MovementState.IDLE
+
+
+func _handle_coyote_time() -> void:
+	if not is_on_floor():
+		movement_state = MovementState.COYOTE_TIME
+
+
 func _on_coyote_timer_timeout():
-	can_jump = false
+	movement_state = MovementState.FALL
 
 
 func _on_jump_queue_timer_timeout():
-	jump_queued = false
+	movement_state = MovementState.FALL
