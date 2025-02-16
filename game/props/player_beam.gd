cb23843d9212ba1d3276aadbc027521d0a38c667
SnappyDragon64 <pg61151@gmail.com>
2025-02-16 08:09:23 +0530

    Level 3 Beam improvements

--- game/props/player_beam.gd
+++ game/props/player_beam.gd
@@ -5,7 +5,7 @@ var player: Player
 
 
 func _physics_process(_delta):
-	if player and not player.player_state == Player.PlayerState.AIM:
+	if player and not player.player_state == Player.PlayerState.AIM and player.is_on_floor():
 		velocity.x = 2.0
 	else:
 		velocity.x = 0.0
